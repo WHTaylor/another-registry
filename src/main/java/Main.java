14754270fd6e0042be4f144c96954f714a6034ed
Will Taylor <william.h.taylor@stfc.ac.uk>
2019-05-21 12:31:56 +0100

    Move infrastructure set up into function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,20 +6,16 @@ import infrastructure.*;
 import projections.Counter;
 import projections.SummaryView;
 
-import java.util.UUID;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        EventStore eventStore = new InMemoryEventStore();
-        AggregateRepo repo = new AggregateRepo(eventStore);
-        repo.setUseCache(false);
-        MessageDispatcher dispatcher = new MessageDispatcher(eventStore, repo);
-        dispatcher.registerAggregateRootClass(Proposal.class);
+        List<Class<? extends AggregateRoot>> aggregateRootClasses = Collections.singletonList(Proposal.class);
         Counter counter = new Counter();
         SummaryView view = new SummaryView();
-        dispatcher.registerEventSubscriber(counter);
-        dispatcher.registerEventSubscriber(view);
-        for (int i = 0; i < 5000000; i++) {
+        List<?> eventSubscribers = Arrays.asList(counter, view);
+        MessageDispatcher dispatcher = setUp(aggregateRootClasses, eventSubscribers);
+        for (int i = 0; i < 500000; i++) {
             UUID id = UUID.randomUUID();
             dispatcher.dispatch(new CreateProposal(id));
             if (i % 10000 == 0) {
@@ -29,7 +25,7 @@ public class Main {
                 dispatcher.dispatch(new SubmitProposal(id, "ref"));
             }
 
-            while(Math.random() > 0.25) {
+            while (Math.random() > 0.25) {
                 dispatcher.dispatch(new ArbitraryCommand(id));
             }
         }
@@ -40,5 +36,24 @@ public class Main {
 
         System.out.println("Average counter was " + view.getAverageCounter() + ", " +
                 "proposal with highest counter was " + view.getSummaryWithHighestCount());
+    }
+
+    private static MessageDispatcher setUp(Collection<Class<? extends AggregateRoot>> aggregateRootClasses,
+                                           Collection<?> eventSubscribers) {
+        EventStore eventStore = new InMemoryEventStore();
+
+        AggregateRepo repo = new AggregateRepo(eventStore);
+        repo.setUseCache(false);
+
+        MessageDispatcher dispatcher = new MessageDispatcher(eventStore, repo);
+        for (Class<? extends AggregateRoot> clazz : aggregateRootClasses) {
+            dispatcher.registerAggregateRootClass(clazz);
+        }
+
+        for (Object subscriber : eventSubscribers) {
+            dispatcher.registerEventSubscriber(subscriber);
+        }
+
+        return dispatcher;
     }
 }
